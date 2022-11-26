7dc793aa17879d81d9d6ee8ec321c74a1429b6dc
Koen Klinkers <k.klinkers@gmail.com>
2022-11-26 16:56:46 +0100

    add default path

--- day-0/src/main.rs
+++ day-0/src/main.rs
@@ -1,6 +1,9 @@
+use std::env;
 use std::fs;
 fn main() {
-    let path = "./input/hello.txt".to_owned();
-    let contents = fs::read_to_string(&path).expect(&("Can't read filed:".to_owned() + &path));
-    println!("{}", contents)
+    let args: Vec<String> = env::args().collect();
+    let fallback = "./input/input.txt".to_owned();
+    let path = &args.get(1).unwrap_or(&fallback);
+    let contents = fs::read_to_string(&path).expect("Can't read file");
+    println!("{}", contents);
 }
