d802b159af140b59633853cb52cf1a064cdf4b0e
Koen Klinkers <k.klinkers@gmail.com>
2022-12-04 15:54:54 +0100

    handle errors

--- day-4/src/pair.rs
+++ day-4/src/pair.rs
@@ -36,13 +36,38 @@ impl FromStr for Pair {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let parsed: Vec<Vec<usize>> = s
             .split(",")
-            .map(|p| p.split("-").map(|c| c.parse::<usize>().unwrap()).collect())
+            .map(|p| {
+                p.split("-")
+                    .filter_map(|c| c.parse::<usize>().ok())
+                    .collect()
+            })
             .collect();
+        if parsed.len() != 2 || parsed[0].len() != 2 || parsed[0].len() != 2 {
+            return Err(PairParseError);
+        }
+
         Ok(Pair {
             ls: parsed[0][0],
             le: parsed[0][1],
             rs: parsed[1][0],
             re: parsed[1][1],
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_pair_success() {
+        assert!("1-2,1-2".parse::<Pair>().is_ok())
+    }
+    #[test]
+    fn test_pair_garbage() {
+        assert!("wrong".parse::<Pair>().is_err())
+    }
+    #[test]
+    fn test_pair_no_int() {
+        assert!("a-5,w-2".parse::<Pair>().is_err())
     }
 }
