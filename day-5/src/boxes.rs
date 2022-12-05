e16d0cb70be38ea696682922717d2252fd0c2c7e
Koen Klinkers <k.klinkers@gmail.com>
2022-12-05 21:03:58 +0100

    added day 5

---
+++ day-5/src/boxes.rs
@@ -0,0 +1,84 @@
+#[derive(Debug)]
+pub struct BoxStack {
+    stacks: Vec<Vec<char>>,
+}
+impl BoxStack {
+    pub fn new(input: &str) -> Self {
+        let mut lines: Vec<&str> = input.split("\n").collect();
+        let length = lines
+            .pop()
+            .unwrap()
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .last()
+            .unwrap()
+            .to_digit(10)
+            .unwrap() as usize;
+
+        let mut stacks = vec![Vec::<char>::new(); length];
+
+        for line in lines.into_iter().rev() {
+            let box_letters: Vec<char> = line.chars().collect();
+            for x in 0..length {
+                if box_letters[1 + x * 4] != ' ' {
+                    stacks[x].push(box_letters[1 + x * 4])
+                }
+            }
+        }
+        BoxStack { stacks }
+    }
+
+    pub fn top(&self) -> String {
+        let mut answer = "".to_string();
+        for stack in self.stacks.iter() {
+            answer.push(stack[stack.len() - 1])
+        }
+        answer
+    }
+
+    pub fn process_9000(&mut self, operation: Operation) {
+        for _ in 0..operation.amount {
+            let letter = self.stacks[operation.source - 1].pop().unwrap();
+            self.stacks[operation.target - 1].push(letter)
+        }
+    }
+    pub fn process_9001(&mut self, operation: Operation) {
+        let l = self.stacks[operation.source - 1].len();
+        let mut letters: Vec<char> = self.stacks[operation.source - 1]
+            .drain(l - operation.amount..l)
+            .collect();
+        self.stacks[operation.target - 1].append(&mut letters);
+    }
+}
+
+#[derive(Debug)]
+pub struct Operation {
+    source: usize,
+    target: usize,
+    amount: usize,
+}
+impl Operation {
+    pub fn new(input: &str) -> Self {
+        let parts: Vec<&str> = input.split(" ").collect();
+        Operation {
+            source: parts[3].parse().unwrap(),
+            target: parts[5].parse().unwrap(),
+            amount: parts[1].parse().unwrap(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_INPUT: &str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 ";
+
+    use super::*;
+    #[test]
+    fn test_stacks() {
+        let stacks = BoxStack::new(&TEST_INPUT.to_string());
+        assert_eq!(stacks.top(), "NDP");
+    }
+}
--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,5 +1,7 @@
+use boxes::{BoxStack, Operation};
 use std::env;
 use std::fs;
+mod boxes;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let fallback = "./input/input.txt".to_owned();
@@ -7,10 +9,38 @@ fn main() {
     let contents = fs::read_to_string(&path).expect("Can't read file");
     let answer = run_1(contents.clone());
     println!("{}", answer);
+    let answer = run_2(contents.clone());
+    println!("{}", answer);
 }
 
 fn run_1(contents: String) -> String {
-    "ABC".to_string()
+    let parts: Vec<&str> = contents.split("\n\n").collect();
+    let mut stacks = BoxStack::new(parts[0]);
+    let operations: Vec<Operation> = parts[1]
+        .trim()
+        .split("\n")
+        .map(|l| Operation::new(l))
+        .collect();
+    for o in operations.into_iter() {
+        stacks.process_9000(o)
+    }
+
+    stacks.top()
+}
+
+fn run_2(contents: String) -> String {
+    let parts: Vec<&str> = contents.split("\n\n").collect();
+    let mut stacks = BoxStack::new(parts[0]);
+    let operations: Vec<Operation> = parts[1]
+        .trim()
+        .split("\n")
+        .map(|l| Operation::new(l))
+        .collect();
+    for o in operations.into_iter() {
+        stacks.process_9001(o)
+    }
+
+    stacks.top()
 }
 #[cfg(test)]
 mod tests {
@@ -28,5 +58,9 @@ move 1 from 1 to 2";
     #[test]
     fn test_day5_1() {
         assert_eq!(run_1(TEST_INPUT.to_string()), "CMZ".to_string())
+    }
+    #[test]
+    fn test_day5_2() {
+        assert_eq!(run_2(TEST_INPUT.to_string()), "MCD".to_string())
     }
 }
